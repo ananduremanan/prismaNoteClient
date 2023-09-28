fb1a919ea9792b524ddd378d44062eb5ee2b0572
ananduremanan <ananduor@gmail.com>
2023-09-28 20:53:10 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const cors = require('cors');
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+const app = express();
+
+app.use(cors()); // Enable All CORS Requests
+app.use(express.json());
+
+app.post('/notes', async (req, res) => {
+  const { title, description } = req.body;
+  const note = await prisma.note.create({
+    data: {
+      title,
+      description,
+    },
+  });
+  res.json({ message: 'Note Created', note });
+});
+
+app.get('/notes', async (req, res) => {
+  const notes = await prisma.note.findMany();
+  res.json({ notes });
+});
+
+app.delete('/notes/:id', async (req, res) => {
+  const { id } = req.params;
+  await prisma.note.delete({
+    where: {
+      id,
+    },
+  });
+  res.json({ message: 'Note Deleted' });
+});
+
+app.listen(3000, () => console.log('Server is running on port 3000'));
