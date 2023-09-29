7436011cff3b26b692f2664a957e5ddd2a5ebeb6
Anandhu Remanan <48993261+ananduremanan@users.noreply.github.com>
2023-09-29 09:10:53 +0000

    Remove Prisma and added mongoose

--- index.js
+++ index.js
@@ -1,36 +1,52 @@
 const express = require('express');
 const cors = require('cors');
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
+const mongoose = require('mongoose');
 const app = express();
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 app.use(cors()); // Enable All CORS Requests
 app.use(express.json());
 
+// Connect to MongoDB
+try{
+  mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+  console.log("Connected to Mongo");
+}catch(error){
+  console.error(error);
+}
+
+// Define Note schema
+const noteSchema = new mongoose.Schema({
+  title: String,
+  description: String,
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now }
+});
+
+// Create Note model
+const Note = mongoose.model('Note', noteSchema);
+
 app.post('/notes', async (req, res) => {
   const { title, description } = req.body;
-  const note = await prisma.note.create({
-    data: {
-      title,
-      description,
-    },
+  const note = new Note({
+    title,
+    description
   });
+  await note.save();
   res.json({ message: 'Note Created', note });
 });
 
 app.get('/notes', async (req, res) => {
   res.statusCode = 200;
-  const notes = await prisma.note.findMany();
+  const notes = await Note.find();
   res.json({ notes });
 });
 
 app.delete('/notes/:id', async (req, res) => {
   const { id } = req.params;
-  await prisma.note.delete({
-    where: {
-      id,
-    },
-  });
+  await Note.findByIdAndDelete(id);
   res.json({ message: 'Note Deleted' });
 });
 
