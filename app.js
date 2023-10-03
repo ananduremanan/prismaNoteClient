f513ab9dcd5d6df6a132a162f99aa2a05fdac7a0
ananduremanan <ananduor@gmail.com>
2023-10-03 19:39:39 +0530

    Revert to Original

--- app.js
+++ app.js
@@ -50,4 +50,4 @@ app.delete('/notes/:id', async (req, res) => {
   res.json({ message: 'Note Deleted' });
 });
 
-module.exports = app;
+app.listen(8080, () => console.log('Server is running on port 3000'));
